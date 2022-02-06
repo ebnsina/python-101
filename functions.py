42b32ce8aa5e51a6769c6579a3098a7bc2b49a36
Ebn Sina <ebnsina.dev@gmail.com>
2022-02-06 23:48:44 +0600

    args and kwargs update

--- functions.py
+++ functions.py
@@ -1,2 +1,25 @@
+
+
 def square(x):
     return x * x
+
+
+# args
+
+def result_args(*args):
+    print(args)
+    return sum(args)
+
+
+print(result_args(10, 20, 30, 40))
+
+# kwargs
+
+
+def result_kwargs(**kwargs):
+    print(kwargs)
+    print(
+        f"Product name {kwargs['name']}, price is {kwargs['price']} and quantity is {kwargs['quantity']}.")
+
+
+print(result_kwargs(name='Apple', price=100, quantity=5))
