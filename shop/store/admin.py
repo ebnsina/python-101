116aea0b3bd0767d7b8bd7c0a9014f15964fd167
Ebn Sina <ebnsina.dev@gmail.com>
2022-03-29 20:37:06 +0600

    shop project init

---
+++ shop/store/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import Category, Product
+
+class CategoryAdmin(admin.ModelAdmin):
+   prepopulated_fields = {'slug': ('title', ) }
+
+
+class ProductAdmin(admin.ModelAdmin):
+   prepopulated_fields = {'slug': ('title', ) }
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
